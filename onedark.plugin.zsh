f46781042afcda2b26be99e425b7c443e52328ab
Jason McClellan <jason.mcclellan@growtherapy.com>
2024-05-13 18:06:23 -0400

    Fix path issue

--- onedark.plugin.zsh
+++ onedark.plugin.zsh
@@ -1,11 +1,3 @@
-#fpath+="${0:A:h}/completions"
-() {
-    local cpath="${0:A:h}/completions"
-
-    [[ -d "$cpath" ]] || mkdir -p "$cpath"
-
-    fpath+="$cpath"
-
-    [[ $commands[pdm] ]] && pdm completion zsh > "$cpath/_pdm"
-}
-
+fpath+="${0:A:h}/completions"
+[[ -d "$cpath" ]] || mkdir -p "$cpath"
+[[ $commands[pdm] ]] && pdm completion zsh > "${0:A:h}/completions/_pdm"
